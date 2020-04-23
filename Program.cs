f0055b244addeb2c3c8616822425691007b613f4
RayLDunbar <rayldunbar@gmail.com>
2020-04-23 19:30:17 -0400

    added json

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 
 namespace DndProject
 {
     class Program
     {
+        static string _monsterRepositoryPath = $"{AppDomain.CurrentDomain.BaseDirectory}\\monsters.json";
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
